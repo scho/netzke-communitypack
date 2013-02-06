be721a22ff0944b13db2cdd773b8e3aa4bde7725
scho <georgmeyer83@googlemail.com>
2013-02-06 14:16:54 +0100

    Switch from mysql to sqlite3

--- config/ci/before-travis.sh
+++ config/ci/before-travis.sh
@@ -9,10 +9,6 @@ mv test/communitypack_test_app/public/extjs-4.1.1 test/communitypack_test_app/pu
 # cp db configuration
 cp test/communitypack_test_app/config/database.yml.travis test/communitypack_test_app/config/database.yml
 
-# create mysql database
-echo 'Create database'
-mysql -e 'create database nct_test;'
-
 echo 'Install communitypack_test_app bundle'
 cd test/communitypack_test_app;
 
